a9118c1ccbfd65414795166e5629fa855775bc69
Maxwelng <maxnunes0711@gmail.com>
2025-12-16 23:22:24 -0300

    Incluindo diretivas no site config

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -5,9 +5,9 @@ export const siteConfig = {
   url: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
   contact: {
     email: process.env.NEXT_PUBLIC_EMAIL || 'facilitehub@virtualassistencia.com.br',
-    whatsapp: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || '5551999999999',
+    whatsapp: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || '551392012884',
   },
   social: {
-    instagram: process.env.NEXT_PUBLIC_INSTAGRAM || 'https://instagram.com',
+    instagram: process.env.NEXT_PUBLIC_INSTAGRAM || 'https://www.instagram.com/facilitehub?igsh=MWl2Y3huajBucDAwdg%3D%3D',
   },
 } as const;

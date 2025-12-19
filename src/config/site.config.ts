36fafa8e5f02d110a11d8b66e3fc70f0bee5d4a7
Maxwelng <maxnunes0711@gmail.com>
2025-12-18 22:11:30 -0300

    Incluindo fav ico e ajustando cel

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
   name: 'Facilite Hub',
-  title: 'Facilite Hub - Assistência Virtual',
+  title: 'Facilite Hub -  Secretária Remota',
   description: 'Soluções estratégicas para mulheres empreendedoras.',
   url: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
   contact: {
     email: process.env.NEXT_PUBLIC_EMAIL || 'facilitehub@virtualassistencia.com.br',
-    whatsapp: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || '551392012884',
+    whatsapp: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || '5511930343922',
   },
   social: {
     instagram: process.env.NEXT_PUBLIC_INSTAGRAM || 'https://www.instagram.com/facilitehub?igsh=MWl2Y3huajBucDAwdg%3D%3D',
